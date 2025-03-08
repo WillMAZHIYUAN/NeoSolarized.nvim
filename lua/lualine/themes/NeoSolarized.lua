4fa353a46c78c82b7e38e97a6b96ff8116bc6efa
WillMAZHIYUAN <codewill@icloud.com>
2025-03-08 12:38:17 +0800

    feat(theme): 主题微调

--- lua/lualine/themes/NeoSolarized.lua
+++ lua/lualine/themes/NeoSolarized.lua
@@ -14,32 +14,32 @@ theme = {
     c = { bg = palette.none, fg = palette.fg0 },
   },
   insert = {
-    a = { bg = palette.real_orange, fg = palette.bg0, gui = 'bold' },
+    a = { fg = palette.real_orange, bg = palette.none, gui = 'bold' },
     b = { bg = palette.none, fg = palette.fg0 },
     c = { bg = palette.none, fg = palette.fg0 },
   },
   visual = {
-    a = { bg = palette.blue, fg = palette.bg0, gui = 'bold' },
+    a = { fg = palette.cyan, bg = palette.none, gui = 'bold' },
     b = { bg = palette.none, fg = palette.fg0 },
     c = { bg = palette.none, fg = palette.fg0 },
   },
   replace = {
-    a = { bg = palette.real_yellow, fg = palette.bg0, gui = 'bold' },
+    a = { fg = palette.real_yellow, bg = palette.none, gui = 'bold' },
     b = { bg = palette.none, fg = palette.fg0 },
     c = { bg = palette.none, fg = palette.fg0 },
   },
   command = {
-    a = { bg = palette.purple, fg = palette.bg0, gui = 'bold' },
+    a = { fg = palette.purple, bg = palette.none, gui = 'bold' },
     b = { bg = palette.none, fg = palette.fg0 },
     c = { bg = palette.none, fg = palette.fg0 },
   },
   terminal = {
-    a = { bg = palette.aqua, fg = palette.bg0, gui = 'bold' },
+    a = { fg = palette.aqua, bg = palette.none, gui = 'bold' },
     b = { bg = palette.none, fg = palette.fg0 },
     c = { bg = palette.none, fg = palette.fg0 },
   },
   inactive = {
-    a = { bg = palette.bg0, fg = palette.fg0, gui = 'bold' },
+    a = { bg = palette.none, fg = palette.none, gui = 'bold' },
     b = { bg = palette.none, fg = palette.fg0 },
     c = { bg = palette.none, fg = palette.fg0 },
   },
